1e7de6afc8a346500d7ba06145cc40c4879b4427
Seth Shaw <seth.shaw@unlv.edu>
2022-06-03 10:37:46 -0700

    increase broken public batch size

--- scripts/delete-broken-public-files.php
+++ scripts/delete-broken-public-files.php
@@ -4,7 +4,7 @@ require 'fedora_user_switch.php';
 
 $files_count = \Drupal::entityQuery('file')->condition('uri','public://%', 'LIKE')->count()->accessCheck(FALSE)->execute();
 print("Checking $files_count files...\n");
-$batch_size = 1000;
+$batch_size = 10000;
 $utils = \Drupal::service('islandora.utils');
 for ($index = 0; $index <= $files_count; $index += $batch_size) {
     $query = \Drupal::entityQuery('file')->condition('uri','public://%', 'LIKE')->range($index,$batch_size)->accessCheck(FALSE);
